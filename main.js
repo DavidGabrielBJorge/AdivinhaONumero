8eff356cf2535aebbf07950edee27aa89a8af7a6
DavidGabrielBJorge <david.jorge@estudante.iftm.edu.br>
2023-01-27 13:18:02 -0300

    add: criando o algoritmo em javascript do jogo

---
+++ main.js
@@ -0,0 +1,68 @@
+let computerNumber;
+let userNumbers = [];
+let attempts = 0;
+let maxguesses = 10;
+let usedNumbers =[];
+let sameNumbers =[];
+
+function newRound(){
+    window.location.reload();
+}
+
+function begin(){
+    computerNumber= Math.floor(Math.random()*100+1);
+    document.getElementById('inputNumber').value='';
+    console.log(computerNumber);
+}
+
+function guessNumbers(){
+    const userNumber = Number(document.getElementById('inputNumber').value);
+
+    userNumbers.push(userNumber);
+
+    if(sameNumbers.includes(userNumber)){
+        sameNumber=userNumbers.pop();
+        document.getElementById('textOutput').innerHTML = 'Você já usou esse número: '+sameNumber;
+    }
+    else{
+        sameNumbers.push(userNumber);
+        document.getElementById('guesses').innerHTML = userNumbers;
+
+        if(userNumber>100){
+            overHundred=userNumbers.pop();
+            document.getElementById('textOutput').innerHTML = 'Tem que ser um número entre 1 e 100';
+        }
+        else{
+            if(attempts < maxguesses){
+                if(userNumber  > computerNumber){
+                    document.getElementById('textOutput').innerHTML = 'Seu número é alto';
+                    document.getElementById('inputNumber').value='';
+                    attempts++;
+                    document.getElementById('attempts').innerHTML=attempts;
+                }
+                else if(userNumber < computerNumber){
+                    document.getElementById('textOutput').innerHTML = 'Seu número é baixo';
+                    document.getElementById('inputNumber').value='';
+                    attempts++;
+                    document.getElementById('attempts').innerHTML=attempts;
+                }
+                
+                else{
+                    document.getElementById('textOutput').innerHTML = 'PARABÉNS VOCÊ GANHOU';
+                    attempts++;
+                    document.getElementById('attempts').innerHTML=attempts;
+                    document.getElementById('inputNumber').setAttribute('Readonly', 'Readonly');
+                }
+            }
+            else{
+                document.getElementById('textOutput').innerHTML = 'VOCÊ PERDEU ! O número era: '+computerNumber ;
+                document.getElementById('inputNumber').setAttribute('Readonly', 'Readonly');
+            }
+        }
+        
+    }
+    document.getElementById('guesses').innerHTML = userNumbers;
+    console.log(userNumber)
+    
+    
+}
